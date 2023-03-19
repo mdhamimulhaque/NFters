65529815d96497c6c0e02240ff280d137e76a4d5
md Hamimul Haue <mdhamimulhaque@gmail.com>
2023-03-20 02:22:11 +0600

    basic setup done

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3D00B7",
+        black: "#000000",
+        line_gray: "#EFEFEF",
+        light_gray: "#C0C0C0",
+        deep_gray: "#565656",
+        text_deep_light_gray: "#696969",
+      },
+
+      fontFamily: {
+        DMSans: ["DM Sans", "sans-serif"],
+        Roboto: ["Roboto", "sans-serif"],
+        IntegralCF: ["Integral CF", "sans-serif"],
+        AvertaDemoPE: ["Averta Demo PE Cutted Demo", "sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+}
